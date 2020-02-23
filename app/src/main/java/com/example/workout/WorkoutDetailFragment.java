3e7b8e7e9c644bc5ea817872b9c8add539c635e4
Majkl <majkl.andracic@gmail.com>
2020-02-23 22:46:48 +0100

    Fragment layout

--- app/src/main/java/com/example/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/workout/WorkoutDetailFragment.java
@@ -11,17 +11,16 @@ import android.view.ViewGroup;
 
 public class WorkoutDetailFragment extends Fragment {
 
-
-    public WorkoutDetailFragment() {
-        // Required empty public constructor
-    }
+    private long workoutId;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 
+    public void setWorkout(long id) {
+        this.workoutId = id;
+    }
 }
