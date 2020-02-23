d01f97eca0d15d0d4c5d91fcc76e5f768c359033
Majkl <majkl.andracic@gmail.com>
2020-02-23 22:43:30 +0100

    Fragment layout

--- app/src/main/java/com/example/workout/Workout.java
+++ app/src/main/java/com/example/workout/Workout.java
@@ -26,15 +26,10 @@ public class Workout {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getDescription() {
         return description;
     }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public String toString(){
+        return this.name;
     }
 }
