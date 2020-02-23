f2776158829be64b1e17856811d46e18f485f5cc
Majkl <majkl.andracic@gmail.com>
2020-02-23 21:39:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/workout/Workout.java
@@ -0,0 +1,40 @@
+package com.example.workout;
+
+public class Workout {
+
+    private String name;
+    private String description;
+
+
+    public static final Workout[] workouts = {
+         new Workout("The Limb Loosener","5 Handstand push-ups\10 " +
+                             "1-legged squats\n15 Pull-ups"),
+         new Workout("Core Agony","100 Pull-ups\n100 Push-ups\n100" +
+                 "Sit-ups\n100 Squats"),
+         new Workout("The Wimp Special","5 Pull-ups\n15 Squats"),
+         new Workout("Strenght and Lenght",
+                 "500 meter run\n21 x 1.5 pood kettleball swing\n21 x pull-ups")
+    };
+
+
+    public Workout(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
