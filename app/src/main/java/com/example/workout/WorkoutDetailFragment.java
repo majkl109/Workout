c84eda6f578ff8f908224dcad02ea6fcdf4167ac
Majkl <majkl.andracic@gmail.com>
2020-02-23 22:19:26 +0100

    Fragment layout

--- app/src/main/java/com/example/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/workout/WorkoutDetailFragment.java
@@ -1,4 +1,27 @@
 package com.example.workout;
 
-public class WorkoutDetailFragment {
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class WorkoutDetailFragment extends Fragment {
+
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
 }
