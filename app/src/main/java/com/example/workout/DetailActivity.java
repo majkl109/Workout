fcda0329a69ceaaed1660ea69ef7e085e04006a7
Majkl <majkl.andracic@gmail.com>
2020-02-24 23:34:41 +0100

    View onStart

--- app/src/main/java/com/example/workout/DetailActivity.java
+++ app/src/main/java/com/example/workout/DetailActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
    // public static final String EXTRA_WORKOUT_ID = "id";
