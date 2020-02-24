84a2e6ca0ee58eb8af4f34a2ea7a8ecf7aea1cf8
Majkl <majkl.andracic@gmail.com>
2020-02-24 23:06:17 +0100

    Fragment set

--- app/src/main/java/com/example/workout/DetailActivity.java
+++ app/src/main/java/com/example/workout/DetailActivity.java
@@ -1,19 +1,18 @@
 package com.example.workout;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
-    public static final String EXTRA_WORKOUT_ID = "id";
+   // public static final String EXTRA_WORKOUT_ID = "id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    //    WorkoutDetailFragment frag = getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        WorkoutDetailFragment frag = (WorkoutDetailFragment)getSupportFragmentManager().findFragmentById(R.id.detail_frag);
     //    int workoutId =(int)getIntent().getExtras().get(EXTRA_WORKOUT_ID);
-    //    frag.setWorkout(workoutId);
+        frag.setWorkout(1);
 
 
     }
