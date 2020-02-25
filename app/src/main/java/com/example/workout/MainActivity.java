a28a2e67db8ca67f39f0175f43bbb5347e91cde7
Majkl <majkl.andracic@gmail.com>
2020-02-25 23:32:45 +0100

    List fragment

--- app/src/main/java/com/example/workout/MainActivity.java
+++ app/src/main/java/com/example/workout/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.workout;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class DetailActivity extends AppCompatActivity {
+public class MainActivity implements WorkoutListFragment.Listener{
    // public static final String EXTRA_WORKOUT_ID = "id";
 
     @Override
@@ -18,8 +15,10 @@ public class DetailActivity extends AppCompatActivity {
         frag.setWorkout(1);
     }
 
-    public void onShowDetails(View view){
+    @Override
+    public void itemClicked(long id){
         Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(MainActivity.EXTRA_WORKOUT_ID, (int)id);
         startActivity(intent);
     }
 }
