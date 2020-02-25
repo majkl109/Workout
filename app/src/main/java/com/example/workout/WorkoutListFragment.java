18d8d2dace6cf995210eedcdeead61f7ca0c5f51
Majkl <majkl.andracic@gmail.com>
2020-02-25 23:20:41 +0100

    List fragment

--- app/src/main/java/com/example/workout/WorkoutListFragment.java
+++ app/src/main/java/com/example/workout/WorkoutListFragment.java
@@ -1,17 +1,23 @@
 package com.example.workout;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import androidx.fragment.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-
-
+import android.widget.ListView;
 
 
 public class WorkoutListFragment extends ListFragment {
+
+    static interface Listener{
+        void itemClicked(long id);
+    }
+
+    private Listener listener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,5 +32,16 @@ public class WorkoutListFragment extends ListFragment {
         setListAdapter(adapter);
 
         return super.onCreateView(inflater,container, savedInstanceState);
+    }
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        this.listener = (Listener)context;
+    }
+    @Override
+    public void onListItemClick(ListView listView, View itemView, int position, long id){
+        if(listener != null){
+            listener.itemClicked(id);
+        }
     }
 }
