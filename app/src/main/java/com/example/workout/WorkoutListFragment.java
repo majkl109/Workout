b019608822e0278a591226cb33a14753fad699c0
Majkl <majkl.andracic@gmail.com>
2020-02-25 23:01:21 +0100

    List fragment

--- app/src/main/java/com/example/workout/WorkoutListFragment.java
+++ app/src/main/java/com/example/workout/WorkoutListFragment.java
@@ -1,25 +1,29 @@
 package com.example.workout;
 
+
 import android.os.Bundle;
+import androidx.fragment.app.ListFragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-import androidx.fragment.app.ListFragment;
+
 
 public class WorkoutListFragment extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedOnInstanceState){
+                             Bundle savedInstanceState){
         String[] names = new String[Workout.workouts.length];
         for(int i = 0; i < names.length; i++){
             names[i] = Workout.workouts[i].getName();
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
-           inflater.getContext(),android.R.layout.simple_list_item_1,names);
+                inflater.getContext(),android.R.layout.simple_list_item_1,names);
         setListAdapter(adapter);
 
         return super.onCreateView(inflater,container, savedInstanceState);
