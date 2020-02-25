d73c5f960619939dc0e313f5ad955fed7e29b6ef
Majkl <majkl.andracic@gmail.com>
2020-02-25 23:11:50 +0100

    List fragment

--- app/src/main/java/com/example/workout/WorkoutListFragment.java
+++ app/src/main/java/com/example/workout/WorkoutListFragment.java
@@ -3,12 +3,11 @@ package com.example.workout;
 
 import android.os.Bundle;
 import androidx.fragment.app.ListFragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
+
 
 
 
