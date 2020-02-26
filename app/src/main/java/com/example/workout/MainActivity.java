fe9f8930f1135b252e30c89a5855e3567ed553b7
Majkl <majkl.andracic@gmail.com>
2020-02-26 23:30:18 +0100

    Main activity update

--- app/src/main/java/com/example/workout/MainActivity.java
+++ app/src/main/java/com/example/workout/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.workout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener{
@@ -15,8 +17,17 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
     }
 
     @Override
-    public void itemClicked(long id){
-
+    public void itemClicked(long id) {
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if (fragmentContainer != null)
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        details.setWorkout(id);
+        ft.replace(R.id.fragment_container, details);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.addToBackStack(null);
+        ft.commit();
+    }else{
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
         startActivity(intent);
